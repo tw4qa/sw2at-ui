a141b06972a7342d2730e638a9e322d7ca4583c6
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-02-07 15:44:32 +0200

    add time detection

--- lib/swat/ui/rspec_setup.rb
+++ lib/swat/ui/rspec_setup.rb
@@ -4,12 +4,14 @@ module Swat
 
       def init_ui(options = {})
         before(:each) do |example|
-
+          @sw_test_started_at = (Time.respond_to?(:now_without_mock_time) ? Time.now_without_mock_time : Time.now)
         end
 
         after(:each) do |example|
-
+          taken =  (Time.respond_to?(:now_without_mock_time) ? Time.now_without_mock_time : Time.now) - @sw_test_started_at
+          puts "'#{example.description}' taken #{Time.at(taken).strftime('%M:%S')}"
         end
+
       end
 
     end
