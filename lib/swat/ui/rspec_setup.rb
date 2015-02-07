a213179ebf8e29b527e0d5c48e0af4dd0b462b4e
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-02-07 16:01:32 +0200

    add stats collector

--- lib/swat/ui/rspec_setup.rb
+++ lib/swat/ui/rspec_setup.rb
@@ -1,6 +1,28 @@
 module Swat
   module UI
     module RspecSetup
+      class StatsCollector
+
+        def collect(example, time)
+          data = example.metadata
+          data[:taken] = time
+          fire_client.push(:test_cases_stats, data)
+        end
+
+        private
+
+        def fire_client
+          require 'firebase'
+          Firebase::Client.new(Swat::UI.config.options[:firebase])
+        end
+
+        def current_branch
+          `git branch | grep '\''*'\'' | awk '\''{print $2}'\''`.gsub("\n",'')
+        rescue
+          nil
+        end
+      end
+
 
       def init_ui(options = {})
         before(:each) do |example|
@@ -9,7 +31,8 @@ module Swat
 
         after(:each) do |example|
           taken =  (Time.respond_to?(:now_without_mock_time) ? Time.now_without_mock_time : Time.now) - @sw_test_started_at
-          puts "'#{example.description}' taken #{Time.at(taken).strftime('%M:%S')}"
+          StatsCollector.new.collect(example, taken) if Swat::UI.config.options[:collect]
+          #puts "'#{example.description}' taken #{Time.at(taken).strftime('%M:%S')}"
         end
 
       end
