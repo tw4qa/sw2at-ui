d5cb94fd356a234a72f980df3895278465ffe527
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-02-08 15:21:57 +0200

    add fire module and model

---
+++ app/models/concerns/fire.rb
@@ -0,0 +1,25 @@
+module Fire
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def all
+      fire_client.get collection
+    end
+
+    def push(attrs)
+      fire_client.push collection, attrs
+    end
+
+    def collection
+      self.to_s
+    end
+
+    def fire_client
+      require 'firebase'
+      @fb ||= Firebase::Client.new(Swat::UI.config.options[:firebase])
+    end
+
+  end
+
+end
---
+++ app/models/test_case.rb
@@ -0,0 +1,4 @@
+class TestCase
+  include Fire
+
+end
