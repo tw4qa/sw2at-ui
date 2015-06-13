0626207def3ee64fc1572d053573270b26b3f04e
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-06-13 20:04:19 +0300

    CL: add formatting to collectio

--- spec/models/calculation_spec.rb
+++ spec/models/calculation_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'Calculations' do
 
   before :all do
-    require Swat::Engine.root.join 'fixtures/full_collections'
+    require Swat::Engine.root.join 'fixtures/firebase_collection'
     Fire.drop!
     Fire.fire_client.set(Fire::ROOT, FULL_COLLECTIONS)
   end
@@ -14,9 +14,9 @@ describe 'Calculations' do
 
   it 'calculate failed statuses' do
     revision_opts = {
-        :branch => "swat-edge",
+        :branch => 'swat-edge',
         :time => DateTime.parse('Wed, 10 Jun 2015 06:27:00 +0000'),
-        :user => "vitaliyt-pc"
+        :user => 'vitaliyt-pc'
     }
     @revision = Revision.query_one(revision_opts)
 
@@ -25,16 +25,16 @@ describe 'Calculations' do
 
     expect(@thread_1).to eq(
       'in_progress' => nil,
-      "status" => { :name=>"completed_failed", :label=>"Failed", failed: true, completed: true },
-      "thread_id" => 0,
-      "thread_name" => "Questions & Submissions",
-      "total_examples" => 20,
-      "total_failed" => 5,
-      "total_pending" => 0,
-      "total_runned" => 20,
+      'status' => { :name=>'completed_failed', :label=>'Failed', failed: true, completed: true },
+      'thread_id' => 0,
+      'thread_name' => 'Questions & Submissions',
+      'total_examples' => 20,
+      'total_failed' => 5,
+      'total_pending' => 0,
+      'total_runned' => 20,
     )
     expect(@revision[:threads].map{|t| t['status'][:name] }).to eq([
-      "completed_failed", "completed_failed", "completed_passed", "completed_passed", "completed_failed"
+      'completed_failed', 'completed_failed', 'completed_passed', 'completed_passed', 'completed_failed'
     ])
 
     expect(@revision[:status][:name]).to eq('completed_failed')
@@ -42,14 +42,14 @@ describe 'Calculations' do
 
   it 'should ' do
     revision_opts = {
-        :branch => "swat-edge",
+        :branch => 'swat-edge',
         :time => DateTime.parse('2015-06-10T07:51:00+00:00'),
-        :user => "vitaliyt-pc"
+        :user => 'vitaliyt-pc'
     }
     @revision = Revision.query_one(revision_opts)
     expect(@revision[:threads].map{|t| t['in_progress'] }).to eq([true]*6)
     expect(@revision[:threads].map{|t| t['status'][:name] }).to eq([
-        "in_progress_success", "in_progress_success", "in_progress_success", "in_progress_success", "in_progress_success", "in_progress_success"
+        'in_progress_success', 'in_progress_success', 'in_progress_success', 'in_progress_success', 'in_progress_success', 'in_progress_success'
     ])
     expect(@revision[:status][:name]).to eq('in_progress_success')
   end
