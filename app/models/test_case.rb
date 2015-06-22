34afadb0965f6f3a7782bef3f81f7aafb127bfa3
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-06-22 15:59:20 +0300

    prevent exception serialization on TestCase object saving

--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -9,14 +9,22 @@ class TestCase < Fire::Model
           full_description: rspec_example.full_description,
           file_path: rspec_example.file_path,
           location: rspec_example.location,
-          exception: rspec_example.exception,
           status: rspec_example.metadata[:execution_result].status,
           started_at: rspec_example.metadata[:execution_result].started_at,
           run_time: rspec_example.metadata[:execution_result].run_time
       }.merge!(revision_opts).merge!(extras)
+
+      if rspec_example.exception
+        data[:exception] = {
+            message: rspec_example.exception.message,
+            backtrace: rspec_example.exception.backtrace,
+        }
+      end
+
       if rspec_example.respond_to?(:swat_extras)
         data.merge!(swat_extras: rspec_example.swat_extras)
       end
+
       create(data)
     end
 
